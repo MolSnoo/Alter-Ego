46c99f9daaae4eb31d62509709c5f3de9152941b
MolSnoo <anderson.jared.16@gmail.com>
2019-10-21 00:35:46 -0400

    Fixed bug where player could use move command while moving and get stuck in infinite loop.

--- Commands/move_player.js
+++ Commands/move_player.js
@@ -24,6 +24,8 @@ module.exports.run = async (bot, game, message, command, args, player) => {
     const status = player.getAttributeStatusEffects("disable move");
     if (status.length > 0) return message.reply(`You cannot do that because you are **${status[0].name}**.`);
 
+    if (player.isMoving) return message.reply(`You cannot do that because you are already moving.`);
+
     var input = args.join(" ");
 
     const currentRoom = player.location;
