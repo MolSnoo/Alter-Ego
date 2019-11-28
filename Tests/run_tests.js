d35d01ae3d572c27641d9efafaa4b6578563d3bd
MolSnoo <anderson.jared.16@gmail.com>
2019-11-28 03:26:50 -0500

    Deleted test_whispers test.

--- Tests/run_tests.js
+++ Tests/run_tests.js
@@ -4,7 +4,6 @@ const test_cell_responses = include(`${settings.testsDir}/test_cell_responses.js
 const test_parse_description = include(`${settings.testsDir}/test_parse_description.js`);
 const test_remove_item = include(`${settings.testsDir}/test_remove_item.js`);
 const test_add_item = include(`${settings.testsDir}/test_add_item.js`);
-const test_whispers = include(`${settings.testsDir}/test_whispers.js`);
 const test_calculate_move_time = include(`${settings.testsDir}/test_calculate_move_time.js`);
 const test_queuer = include(`${settings.testsDir}/test_queuer.js`);
 
@@ -13,7 +12,6 @@ exports.runTests = async function (bot) {
     test_parse_description.run();
     //test_remove_item.run();
     //test_add_item.run();
-    //await test_whispers.run(bot);
     //test_calculate_move_time.run();
     test_queuer.run();
     console.log("All tests passed.");
