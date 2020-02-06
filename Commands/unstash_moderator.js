7867235f8ce8e5ec3dd9ac9adf792ba559415aef
MolSnoo <anderson.jared.16@gmail.com>
2020-02-06 18:38:31 -0500

    Added unstash_moderator command.

--- Commands/unstash_moderator.js
+++ Commands/unstash_moderator.js
@@ -1,29 +1,37 @@
 const settings = include('settings.json');
 
 module.exports.config = {
-    name: "unstash_player",
-    description: "Moves an inventory item into your hand.",
-    details: "Moves an inventory item from another item in your inventory into your hand. You can specify which item to remove it from, if you have "
+    name: "unstash_moderator",
+    description: "Moves an inventory item into a player's hand.",
+    details: "Moves a player's inventory item from another item in their inventory into their hand. You can specify which item to remove it from, if they have "
         + "multiple items with the same name. If the inventory item you choose to move it from has multiple slots for items (such as multiple pockets), "
         + "you can specify which slot you want to take it from as well. If you attempt to unstash a very large item (a sword, for example), "
-        + "people in the room with you will see you doing so.",
-    usage: `${settings.commandPrefix}unstash laptop\n`
-        + `${settings.commandPrefix}retrieve sword from sheath\n`
-        + `${settings.commandPrefix}unstash old key from right pocket of pants\n`
-        + `${settings.commandPrefix}retrieve water bottle from side pouch of backpack`,
-    usableBy: "Player",
-    aliases: ["unstash", "retrieve"]
+        + "people in the room with the player will see them doing so.",
+    usage: `${settings.commandPrefix}unstash vivian's laptop\n`
+        + `${settings.commandPrefix}retrieve nero sword from sheath\n`
+        + `${settings.commandPrefix}unstash antimony's old key from right pocket of pants\n`
+        + `${settings.commandPrefix}retrieve cassie water bottle from side pouch of backpack`,
+    usableBy: "Moderator",
+    aliases: ["unstash", "retrieve"],
+    requiresGame: true
 };
 
-module.exports.run = async (bot, game, message, command, args, player) => {
-    if (args.length === 0) {
-        message.reply("you need to specify an item. Usage:");
+module.exports.run = async (bot, game, message, command, args) => {
+    if (args.length < 2) {
+        message.reply("you need to specify a player and an item. Usage:");
         message.channel.send(exports.config.usage);
         return;
     }
 
-    const status = player.getAttributeStatusEffects("disable unstash");
-    if (status.length > 0) return message.reply(`You cannot do that because you are **${status[0].name}**.`);
+    var player = null;
+    for (let i = 0; i < game.players_alive.length; i++) {
+        if (game.players_alive[i].name.toLowerCase() === args[0].toLowerCase().replace(/'s/g, "")) {
+            player = game.players_alive[i];
+            args.splice(0, 1);
+            break;
+        }
+    }
+    if (player === null) return message.reply(`player "${args[0]}" not found.`);
 
     // First, check if the player has a free hand.
     var hand = "";
@@ -40,8 +48,8 @@ module.exports.run = async (bot, game, message, command, args, player) => {
         else if (player.inventory[slot].name === "LEFT HAND")
             break;
     }
-    if (hand === "") return message.reply("you do not have a free hand to retrieve an item. Either drop an item you're currently holding or stash it in one of your equipped items.");
-    
+    if (hand === "") return message.reply(`${player.name} does not have a free hand to retrieve an item.`);
+
     var input = args.join(' ');
     var parsedInput = input.toUpperCase().replace(/\'/g, "");
     var newArgs = parsedInput.split(" FROM ");
@@ -63,7 +71,7 @@ module.exports.run = async (bot, game, message, command, args, player) => {
                 if (items[i].container !== null && items[i].container.name === containerName)
                     matches.push({ container: items[i].container, slot: items[i].slot, item: items[i] });
             }
-            if (matches.length === 0) return message.reply(`couldn't find "${containerName}" in your inventory containing "${itemName}".`);
+            if (matches.length === 0) return message.reply(`couldn't find "${containerName}" in ${player.name}'s inventory containing "${itemName}".`);
 
             // Slot name was specified.
             if (slotName !== "" && slotName !== null && slotName !== undefined) {
@@ -76,7 +84,7 @@ module.exports.run = async (bot, game, message, command, args, player) => {
                         }
                     }
                 }
-                if (container === null) return message.reply(`couldn't find "${containerName}" in your inventory with inventory slot "${slotName}".`);
+                if (container === null) return message.reply(`couldn't find "${containerName}" in ${player.name}'s inventory with inventory slot "${slotName}".`);
             }
             // Slot name wasn't specified. Pick the first container.
             else {
@@ -92,13 +100,15 @@ module.exports.run = async (bot, game, message, command, args, player) => {
             slotName = item ? item.slot : null;
         }
     }
-    if (items.length === 0 || item === null || item === undefined) return message.reply(`couldn't find item "${itemName}" in your inventory.`);
+    if (items.length === 0 || item === null || item === undefined) return message.reply(`couldn't find item "${itemName}" in ${player.name}'s inventory.`);
     if (item !== null && item !== undefined && container === null) return message.reply(`${item.name} is not contained in another item and cannot be unstashed.`);
 
     player.unstash(game, item, hand, container, slotName);
     // Post log message.
     const time = new Date().toLocaleTimeString();
-    game.logChannel.send(`${time} - ${player.name} unstashed ${item.name} from ${slotName} of ${container.name} in ${player.location.channel}`);
+    game.logChannel.send(`${time} - ${player.name} forcefully unstashed ${item.name} from ${slotName} of ${container.name} in ${player.location.channel}`);
+
+    message.channel.send(`Successfully unstashed ${item.name} from ${slotName} of ${container.name} for ${player.name}.`);
 
     return;
 };
