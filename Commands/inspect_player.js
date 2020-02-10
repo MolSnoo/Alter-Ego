775df097d3f0e31acc4499e702a4868775b17e6d
MolSnoo <anderson.jared.16@gmail.com>
2020-02-10 01:40:16 -0500

    Updated inspect command to inspect other players and their items.

--- Commands/inspect_player.js
+++ Commands/inspect_player.js
@@ -5,17 +5,23 @@ const Narration = include(`${settings.dataDir}/Narration.js`);
 
 module.exports.config = {
     name: "inspect_player",
-    description: "Learn more about an object or item.",
-    details: 'Tells you about an object or item in the room you\'re in. The description will be sent to you via DMs. '
+    description: "Learn more about an object, item, or player.",
+    details: 'Tells you about an object, item, or player in the room you\'re in. The description will be sent to you via DMs. '
         + 'An object is something in the room that you can interact with but not take with you. '
         + 'An item is something that you can both interact with and take with you. If you inspect an object, '
         + 'everyone in the room will see you inspect it. The same goes for very large items. '
         + 'You can also inspect items in your inventory. If you have an item with the same name as an item in the room you\'re currently in, '
         + 'you can specify that you want to inspect your item by adding "my" before the item name. '
+        + 'You can even inspect visible items in another player\'s inventory by adding "[player name]\'s" before the item name. No one will '
+        + 'see you do this, however you will receive slightly less info when inspecting another player\'s items. '
         + `You can use "${settings.commandPrefix}inspect room" to get the description of the room you're currently in.`,
     usage: `${settings.commandPrefix}inspect desk\n`
         + `${settings.commandPrefix}examine knife\n`
         + `${settings.commandPrefix}investigate my knife\n`
+        + `${settings.commandPrefix}look faust\n`
+        + `${settings.commandPrefix}examine an individual wearing a mask\n`
+        + `${settings.commandPrefix}look marielle's glasses\n`
+        + `${settings.commandPrefix}investigate an individual wearing a bucket's shirt\n`
         + `${settings.commandPrefix}inspect room`,
     usableBy: "Player",
     aliases: ["inspect", "investigate", "examine", "look"]
@@ -100,10 +106,10 @@ module.exports.run = async (bot, game, message, command, args, player) => {
         }
     }
 
-    // Finally, check if the input is an item in the player's inventory.
+    // Check if the input is an item in the player's inventory.
     const inventory = game.inventoryItems.filter(item => item.player.id === player.id && item.prefab !== null);
     for (let i = 0; i < inventory.length; i++) {
-        parsedInput = parsedInput.replace("MY ", "");
+        parsedInput = parsedInput.replace("MY ", "").replace(`${player.name.toUpperCase()}S `, "");
         if (inventory[i].prefab.name === parsedInput && inventory[i].quantity > 0) {
             const item = inventory[i];
             if (!item.prefab.discreet) new Narration(game, player, player.location, `${player.displayName} takes out ${item.prefab.singleContainingPhrase} and begins inspecting it.`).send();
@@ -113,6 +119,49 @@ module.exports.run = async (bot, game, message, command, args, player) => {
             game.logChannel.send(`${time} - ${player.name} inspected ${item.prefab.id} from their inventory in ${player.location.channel}`);
 
             return;
+        }
+    }
+
+    // Check if the input is a player in the room.
+    for (let i = 0; i < player.location.occupants.length; i++) {
+        let occupant = player.location.occupants[i];
+        const possessive = occupant.displayName.toUpperCase() + "S ";
+        if (occupant.displayName.toUpperCase() === parsedInput) {
+            // Don't let player inspect themselves.
+            if (occupant.id === player.id) return message.reply(`can't inspect yourself.`);
+            player.sendDescription(occupant.description, occupant);
+
+            const time = new Date().toLocaleTimeString();
+            game.logChannel.send(`${time} - ${player.name} inspected ${occupant.name} in ${player.location.channel}`);
+
+            return;
+        }
+        else if (parsedInput.startsWith(possessive)) {
+            // Don't let the player inspect their own items this way.
+            if (occupant.id === player.id) return message.reply(`can't inspect your own items this way. Use "my" instead of your name.`);
+            parsedInput = parsedInput.replace(possessive, "");
+            // Only equipped items should be an option.
+            const inventory = game.inventoryItems.filter(item => item.player.id === occupant.id && item.prefab !== null && item.containerName === "" && item.container === null);
+            for (let j = 0; j < inventory.length; j++) {
+                if (inventory[j].prefab.name === parsedInput && (inventory[j].equipmentSlot !== "LEFT HAND" && inventory[j].equipmentSlot !== "RIGHT HAND" || !inventory[j].prefab.discreet)) {
+                    // Make sure the item isn't covered by anything first.
+                    const coveringItems = inventory.filter(item =>
+                        item.equipmentSlot !== "RIGHT HAND" &&
+                        item.equipmentSlot !== "LEFT HAND" &&
+                        item.prefab.coveredEquipmentSlots.includes(inventory[j].equipmentSlot)
+                    );
+                    if (coveringItems.length === 0) {
+                        // Clear out any il tags in the description.
+                        let description = inventory[j].description.replace(/(<(il)(\s[^>]+?)*>)[\s\S]+?(<\/\2>)/g, "$1$4");
+                        player.sendDescription(description, inventory[j]);
+
+                        const time = new Date().toLocaleTimeString();
+                        game.logChannel.send(`${time} - ${player.name} inspected ${inventory[j].prefab.id} from ${occupant.name}'s inventory in ${player.location.channel}`);
+
+                        return;
+                    }
+                }
+            }
         }
     }
 
