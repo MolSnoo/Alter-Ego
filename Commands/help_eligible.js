8c2d86b81c02f6b20f9216eed1921266ce8cd877
MolSnoo <anderson.jared.16@gmail.com>
2020-02-20 18:11:19 -0500

    Added pagination to help menus.

--- Commands/help_eligible.js
+++ Commands/help_eligible.js
@@ -23,16 +23,55 @@ module.exports.run = async (bot, game, message, args, player) => {
     });
 
     if (args.length === 0) {
-        let embed = new discord.RichEmbed()
-            .setColor('1F8B4C')
-            .setAuthor(`${game.guild.me.displayName} Help`, game.guild.iconURL)
-            .setDescription(`These are the available commands for users with the Student role.`);
+        var fields = [];
+        var pages = [];
+        var page = 0;
 
         roleCommands.forEach(function (value, key, map) {
             const commandName = key.substring(0, key.indexOf('_'));
-            embed.addField(`${settings.commandPrefix}${commandName}`, value.description);
+            fields.push({ command: `${settings.commandPrefix}${commandName}`, description: value.description });
         });
-        message.channel.send(embed);
+
+        // Divide the fields into pages.
+        for (let i = 0, pageNo = 0; i < fields.length; i++) {
+            // Divide the menu into groups of 10.
+            if (i % 10 === 0) {
+                pages.push([]);
+                if (i !== 0) pageNo++;
+            }
+            pages[pageNo].push(fields[i]);
+        }
+
+        let embed = createEmbed(game, page, pages);
+        message.channel.send(embed).then(msg => {
+            msg.react('⏪').then(() => {
+                msg.react('⏩');
+
+                const backwardsFilter = (reaction, user) => reaction.emoji.name === '⏪' && user.id === message.author.id;
+                const forwardsFilter = (reaction, user) => reaction.emoji.name === '⏩' && user.id === message.author.id;
+
+                const backwards = msg.createReactionCollector(backwardsFilter, { time: 60000 });
+                const forwards = msg.createReactionCollector(forwardsFilter, { time: 60000 });
+
+                backwards.on("collect", () => {
+                    const reaction = msg.reactions.find(reaction => reaction.emoji.name === '⏪');
+                    if (reaction) reaction.users.forEach(user => { if (user.id !== bot.user.id) reaction.remove(user.id); });
+                    if (page === 0) return;
+                    page--;
+                    embed = createEmbed(game, page, pages);
+                    msg.edit(embed);
+                });
+
+                forwards.on("collect", () => {
+                    const reaction = msg.reactions.find(reaction => reaction.emoji.name === '⏩');
+                    if (reaction) reaction.users.forEach(user => { if (user.id !== bot.user.id) reaction.remove(user.id); });
+                    if (page === pages.length - 1) return;
+                    page++;
+                    embed = createEmbed(game, page, pages);
+                    msg.edit(embed);
+                });
+            });
+        });
     }
     else {
         let command = roleCommands.find(command => command.aliases.includes(args[0]));
@@ -56,3 +95,20 @@ module.exports.run = async (bot, game, message, args, player) => {
 
     return;
 };
+
+function createEmbed(game, page, pages) {
+    const roleId = settings.debug ? settings.testerRole : settings.eligibleRole;
+    const role = game.guild.roles.get(roleId);
+    const roleName = role ? role.name : "Eligible";
+    let embed = new discord.RichEmbed()
+        .setColor('1F8B4C')
+        .setAuthor(`${game.guild.me.displayName} Help`, game.guild.iconURL)
+        .setDescription(`These are the available commands for users with the ${roleName} role.\nSend \`${settings.commandPrefix}help commandname\` for more details.`)
+        .setFooter(`Page ${page + 1}/${pages.length}`);
+
+    // Now add the fields of the first page.
+    for (let i = 0; i < pages[page].length; i++)
+        embed.addField(pages[page][i].command, pages[page][i].description);
+
+    return embed;
+}
--- Commands/help_player.js
+++ Commands/help_player.js
@@ -22,16 +22,51 @@ module.exports.run = async (bot, game, message, command, args, player) => {
     });
 
     if (args.length === 0) {
-        let embed = new discord.RichEmbed()
-            .setColor('1F8B4C')
-            .setAuthor(`${game.guild.me.displayName} Help`, game.guild.iconURL)
-            .setDescription(`These are the available commands for users with the Player role.`);
+        var fields = [];
+        var pages = [];
+        var page = 0;
 
         roleCommands.forEach(function (value, key, map) {
             const commandName = key.substring(0, key.indexOf('_'));
-            embed.addField(`${settings.commandPrefix}${commandName}`, value.description);
+            fields.push({ command: `${settings.commandPrefix}${commandName}`, description: value.description });
         });
-        message.channel.send(embed);
+
+        // Divide the fields into pages.
+        for (let i = 0, pageNo = 0; i < fields.length; i++) {
+            // Divide the menu into groups of 10.
+            if (i % 10 === 0) {
+                pages.push([]);
+                if (i !== 0) pageNo++;
+            }
+            pages[pageNo].push(fields[i]);
+        }
+
+        let embed = createEmbed(game, page, pages);
+        message.author.send(embed).then(msg => {
+            msg.react('⏪').then(() => {
+                msg.react('⏩');
+
+                const backwardsFilter = (reaction, user) => reaction.emoji.name === '⏪' && user.id === message.author.id;
+                const forwardsFilter = (reaction, user) => reaction.emoji.name === '⏩' && user.id === message.author.id;
+
+                const backwards = msg.createReactionCollector(backwardsFilter, { time: 60000 });
+                const forwards = msg.createReactionCollector(forwardsFilter, { time: 60000 });
+
+                backwards.on("collect", () => {
+                    if (page === 0) return;
+                    page--;
+                    embed = createEmbed(game, page, pages);
+                    msg.edit(embed);
+                });
+
+                forwards.on("collect", () => {
+                    if (page === pages.length - 1) return;
+                    page++;
+                    embed = createEmbed(game, page, pages);
+                    msg.edit(embed);
+                });
+            });
+        });
     }
     else {
         let command = roleCommands.find(command => command.aliases.includes(args[0]));
@@ -55,3 +90,19 @@ module.exports.run = async (bot, game, message, command, args, player) => {
 
     return;
 };
+
+function createEmbed(game, page, pages) {
+    const role = game.guild.roles.get(settings.playerRole);
+    const roleName = role ? role.name : "Player";
+    let embed = new discord.RichEmbed()
+        .setColor('1F8B4C')
+        .setAuthor(`${game.guild.me.displayName} Help`, game.guild.iconURL)
+        .setDescription(`These are the available commands for users with the ${roleName} role.\nSend \`${settings.commandPrefix}help commandname\` for more details.`)
+        .setFooter(`Page ${page + 1}/${pages.length}`);
+
+    // Now add the fields of the first page.
+    for (let i = 0; i < pages[page].length; i++)
+        embed.addField(pages[page][i].command, pages[page][i].description);
+
+    return embed;
+}
