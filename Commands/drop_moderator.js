cc0b38765a9fa109553dfd0c9a9213ffa44568ea
MolSnoo <anderson.jared.16@gmail.com>
2020-02-23 03:14:53 -0500

    Fixed drop_moderator command.

--- Commands/drop_moderator.js
+++ Commands/drop_moderator.js
@@ -39,23 +39,50 @@ module.exports.run = async (bot, game, message, command, args) => {
     var parsedInput = input.toUpperCase().replace(/\'/g, "");
     var newArgs = null;
 
+    // First, find the item in the player's inventory.
+    var item = null;
+    var hand = "";
+    for (let slot = 0; slot < player.inventory.length; slot++) {
+        if (player.inventory[slot].equippedItem !== null && (parsedInput.startsWith(player.inventory[slot].equippedItem.name + ' ') || player.inventory[slot].equippedItem.name === parsedInput)) {
+            if (player.inventory[slot].name === "RIGHT HAND" && player.inventory[slot].equippedItem !== null) {
+                item = player.inventory[slot].equippedItem;
+                hand = "RIGHT HAND";
+                break;
+            }
+            else if (player.inventory[slot].name === "LEFT HAND" && player.inventory[slot].equippedItem !== null) {
+                item = player.inventory[slot].equippedItem;
+                hand = "LEFT HAND";
+                break;
+            }
+        }
+        // If it's reached the left hand and it doesn't have the desired item, neither hand has it. Stop looking.
+        else if (player.inventory[slot].name === "LEFT HAND")
+            break;
+    }
+    if (item !== null) {
+        parsedInput = parsedInput.substring(item.name.length).trim();
+        newArgs = parsedInput.split(' ');
+    }
+    else return message.reply(`couldn't find item "${parsedInput}" in either of ${player.name}'s hands.`);
+
     // Check if an object was specified.
     const objects = game.objects.filter(object => object.location.name === player.location.name && object.accessible);
     var object = null;
-    for (let i = 0; i < objects.length; i++) {
-        if (objects[i].name === parsedInput) return message.reply(`you need to specify an item for ${player.name} to drop.`);
-        if (parsedInput.endsWith(objects[i].name)) {
-            if (objects[i].preposition === "") return message.reply(`${objects[i].name} cannot hold items.`);
-            object = objects[i];
-            parsedInput = parsedInput.substring(0, parsedInput.lastIndexOf(objects[i].name)).trimEnd();
-            // Check if the object has a puzzle attached to it.
-            if (object.childPuzzle !== null && (!object.childPuzzle.accessible || !object.childPuzzle.solved))
-                return message.reply(`you cannot put items ${object.preposition} ${object.name} right now.`);
-            newArgs = parsedInput.split(' ');
-            var objectPreposition = newArgs[newArgs.length - 1].toLowerCase();
-            newArgs.splice(newArgs.length - 1, 1);
-            parsedInput = newArgs.join(' ');
-            break;
+    if (parsedInput !== "") {
+        for (let i = 0; i < objects.length; i++) {
+            if (objects[i].name === parsedInput) return message.reply(`you need to supply a preposition.`);
+            if ((parsedInput === `${objects[i].preposition.toUpperCase()} ${objects[i].name}` || parsedInput === `IN ${objects[i].name}`) && objects[i].preposition !== "") {
+                object = objects[i];
+                parsedInput = parsedInput.substring(0, parsedInput.lastIndexOf(objects[i].name)).trimEnd();
+                // Check if the object has a puzzle attached to it.
+                if (object.childPuzzle !== null && (!object.childPuzzle.accessible || !object.childPuzzle.solved))
+                    return message.reply(`you cannot put items ${object.preposition} ${object.name} right now.`);
+                newArgs = parsedInput.split(' ');
+                newArgs.splice(newArgs.length - 1, 1);
+                parsedInput = newArgs.join(' ');
+                break;
+            }
+            else if (parsedInput === `${newArgs[0]} ${objects[i].name}` && objects[i].preposition === "") return message.reply(`${objects[i].name} cannot hold items.`);
         }
     }
 
@@ -63,53 +90,35 @@ module.exports.run = async (bot, game, message, command, args) => {
     var items = game.items.filter(item => item.location.name === player.location.name && item.accessible && (item.quantity > 0 || isNaN(item.quantity)));
     var containerItem = null;
     var containerItemSlot = null;
-    for (let i = 0; i < items.length; i++) {
-        if (parsedInput.endsWith(items[i].name) && parsedInput !== items[i].name) {
-            if (object === null || object !== null && items[i].container !== null && (items[i].container.name === object.name || items[i].container.hasOwnProperty("parentObject") && items[i].container.parentObject.name === object.name)) {
-                if (items[i].inventory.length === 0) return message.reply(`${items[i].name} cannot hold items.`);
-                containerItem = items[i];
-                parsedInput = parsedInput.substring(0, parsedInput.lastIndexOf(items[i].name)).trimEnd();
-                // Check if a slot was specified.
-                if (parsedInput.endsWith(" OF")) {
-                    parsedInput = parsedInput.substring(0, parsedInput.lastIndexOf(" OF")).trimEnd();
-                    newArgs = parsedInput.split(' ');
-                    for (let slot = 0; slot < containerItem.inventory.length; slot++) {
-                        if (parsedInput.endsWith(containerItem.inventory[slot].name)) {
-                            containerItemSlot = containerItem.inventory[slot];
-                            parsedInput = parsedInput.substring(0, parsedInput.lastIndexOf(containerItemSlot.name)).trimEnd();
-                            break;
+    if (parsedInput !== "") {
+        for (let i = 0; i < items.length; i++) {
+            if (items[i].name === parsedInput) return message.reply(`you need to supply a preposition.`);
+            if (parsedInput.endsWith(items[i].name) && parsedInput !== items[i].name) {
+                if (object === null || object !== null && items[i].container !== null && (items[i].container.name === object.name || items[i].container.hasOwnProperty("parentObject") && items[i].container.parentObject.name === object.name)) {
+                    if (items[i].inventory.length === 0) return message.reply(`${items[i].name} cannot hold items.`);
+                    containerItem = items[i];
+                    parsedInput = parsedInput.substring(0, parsedInput.lastIndexOf(items[i].name)).trimEnd();
+                    // Check if a slot was specified.
+                    if (parsedInput.endsWith(" OF")) {
+                        parsedInput = parsedInput.substring(0, parsedInput.lastIndexOf(" OF")).trimEnd();
+                        newArgs = parsedInput.split(' ');
+                        for (let slot = 0; slot < containerItem.inventory.length; slot++) {
+                            if (parsedInput.endsWith(containerItem.inventory[slot].name)) {
+                                containerItemSlot = containerItem.inventory[slot];
+                                parsedInput = parsedInput.substring(0, parsedInput.lastIndexOf(containerItemSlot.name)).trimEnd();
+                                break;
+                            }
                         }
+                        if (containerItemSlot === null) return message.reply(`couldn't find "${newArgs[newArgs.length - 1]}" of ${containerItem.name}.`);
                     }
-                    if (containerItemSlot === null) return message.reply(`couldn't find "${newArgs[newArgs.length - 1]}" of ${containerItem.name}.`);
+                    newArgs = parsedInput.split(' ');
+                    newArgs.splice(newArgs.length - 1, 1);
+                    parsedInput = newArgs.join(' ');
+                    break;
                 }
-                newArgs = parsedInput.split(' ');
-                var itemPreposition = newArgs[newArgs.length - 1].toLowerCase();
-                newArgs.splice(newArgs.length - 1, 1);
-                parsedInput = newArgs.join(' ');
-                break;
             }
         }
     }
-
-    // Now find the item in the player's inventory.
-    var item = null;
-    var hand = "";
-    for (let slot = 0; slot < player.inventory.length; slot++) {
-        if (player.inventory[slot].name === "RIGHT HAND" && player.inventory[slot].equippedItem !== null && player.inventory[slot].equippedItem.name === parsedInput) {
-            item = player.inventory[slot].equippedItem;
-            hand = "RIGHT HAND";
-            break;
-        }
-        else if (player.inventory[slot].name === "LEFT HAND" && player.inventory[slot].equippedItem !== null && player.inventory[slot].equippedItem.name === parsedInput) {
-            item = player.inventory[slot].equippedItem;
-            hand = "LEFT HAND";
-            break;
-        }
-        // If it's reached the left hand and it doesn't have the desired item, neither hand has it. Stop looking.
-        else if (player.inventory[slot].name === "LEFT HAND")
-            break;
-    }
-    if (item === null) return message.reply(`couldn't find item "${parsedInput}" in either of ${player.name}'s hands.`);
 
     // Now decide what the container should be.
     var container = null;
@@ -128,6 +137,7 @@ module.exports.run = async (bot, game, message, command, args) => {
         else if (containerItemSlot.takenSpace + item.prefab.size > containerItemSlot.capacity) return message.reply(`${item.name} will not fit in ${container.name} because there isn't enough space left.`);
     }
     else {
+        if (parsedInput !== "") return message.reply(`couldn't find "${parsedInput}" to drop item into.`);
         const defaultDropOpject = objects.find(object => object.name === settings.defaultDropObject);
         if (defaultDropOpject === null || defaultDropOpject === undefined) return message.reply(`there is no default drop object "${settings.defaultDropObject}" in ${player.location.name}.`);
         container = defaultDropOpject;
