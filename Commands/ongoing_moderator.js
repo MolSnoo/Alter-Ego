224cd955b61d825deb35adf32fbafdbf9aef7a7c
MolSnoo <molsno.vivian@gmail.com>
2022-05-15 16:26:30 -0400

    Added ongoing_moderator command.

---
+++ Commands/ongoing_moderator.js
@@ -0,0 +1,28 @@
+const settings = include('settings.json');
+
+module.exports.config = {
+    name: "ongoing_moderator",
+    description: "Lists all ongoing events.",
+    details: "Lists all events which are currently ongoing, along with the time remaining on each one, if applicable.",
+    usage: `${settings.commandPrefix}ongoing\n`
+        + `${settings.commandPrefix}events`,
+    usableBy: "Moderator",
+    aliases: ["ongoing", "events"],
+    requiresGame: true
+};
+
+module.exports.run = async (bot, game, message, command, args) => {
+    var events = [];
+    for (let i = 0; i < game.events.length; i++) {
+        if (game.events[i].ongoing) {
+            if (game.events[i].remaining === null)
+                events.push(game.events[i].name);
+            else
+                events.push(game.events[i].name + ` (${game.events[i].remainingString})`);
+        }
+    }
+    const eventList = events.join(", ");
+    game.messageHandler.addGameMechanicMessage(message.channel, `Ongoing events:\n${eventList}`);
+
+    return;
+};
