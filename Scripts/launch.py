a53b765c561e6bbc79c3c1260aa4646db457f5aa
Amy Poon <amy@fluffles.me>
2024-01-12 22:03:44 -0500

    fix launch.py again

--- Scripts/launch.py
+++ Scripts/launch.py
@@ -3,12 +3,12 @@ import sys
 from os import environ
 import write_config
 
-image_commit = environ.get("IMAGE_COMMIT").split("}",1)[0]
-
 if environ.get("IMAGE_TAG") is not None:
+    image_commit = environ.get("IMAGE_COMMIT").split("}",1)[0]
     image_tag = environ.get("IMAGE_TAG").split("}",1)[0]
     print(f"Alter Ego {image_tag.split(':',1)[1]} (commit {image_commit})\n")
 else:
+    image_commit = environ.get("IMAGE_COMMIT")
     print(f"Alter Ego (commit {image_commit})\n")
 
 print("Writing configuration files...")
