8b6440f613c9486e14bede8c33d25fb5d52c371c
Amy Poon <amy@fluffles.me>
2024-01-12 21:34:30 -0500

    split tag to get version only

--- Scripts/launch.py
+++ Scripts/launch.py
@@ -7,7 +7,7 @@ image_commit = environ.get("IMAGE_COMMIT")
 image_tag = environ.get("IMAGE_TAG")
 
 if image_tag is not None:
-    print(f"Alter Ego {image_tag} (build {image_commit})\n")
+    print(f"Alter Ego {image_tag.split(':',1)[1]} (build {image_commit})\n")
 else:
     print(f"Alter Ego (build {image_commit})\n")
 
