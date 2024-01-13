0d00c12ef3426f87a3239ef57c60d3aefb9c353e
Amy Poon <amy@fluffles.me>
2024-01-12 22:01:38 -0500

    bugfix launch.py

--- Scripts/launch.py
+++ Scripts/launch.py
@@ -4,9 +4,9 @@ from os import environ
 import write_config
 
 image_commit = environ.get("IMAGE_COMMIT").split("}",1)[0]
-image_tag = environ.get("IMAGE_TAG").split("}",1)[0]
 
-if image_tag is not None:
+if environ.get("IMAGE_TAG") is not None:
+    image_tag = environ.get("IMAGE_TAG").split("}",1)[0]
     print(f"Alter Ego {image_tag.split(':',1)[1]} (commit {image_commit})\n")
 else:
     print(f"Alter Ego (commit {image_commit})\n")
