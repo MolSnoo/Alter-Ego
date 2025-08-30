dd76f79c12c31b49a27853a31327353b12d79bb7
Samantha Divina <lavcorps@protonmail.com>
2025-08-30 13:28:10 -0600

    implement check for disabling time command

--- Commands/time_player.js
+++ Commands/time_player.js
@@ -10,6 +10,9 @@ module.exports.config = {
 };
 
 module.exports.run = async (bot, game, message, command, args, player) => {
+    const status = player.getAttributeStatusEffects("disable time");
+    if (status.length > 0) return game.messageHandler.addReply(message, `You cannot do that because you are **${status[0].name}**.`);
+
     const timeMessage = `The time is **${new Date().toLocaleTimeString()}**`;
     game.messageHandler.addGameMechanicMessage(player.member, timeMessage);
 
