ca826145ecc04320593aac696195061e4d7d3cc0
Samantha Divina <lavcorps@protonmail.com>
2025-08-30 16:19:55 -0600

    implement data changes for uncrafting

--- Data/Recipe.js
+++ Data/Recipe.js
@@ -1,13 +1,15 @@
 const constants = include('Configs/constants.json');
 
 class Recipe {
-    constructor(ingredients, objectTag, duration, products, initiatedDescription, completedDescription, row) {
+    constructor(ingredients, uncraftable, objectTag, duration, products, initiatedDescription, completedDescription, uncraftedDescription, row) {
         this.ingredients = ingredients;
+        this.uncraftable = uncraftable;
         this.objectTag = objectTag;
         this.duration = duration;
         this.products = products;
         this.initiatedDescription = initiatedDescription;
         this.completedDescription = completedDescription;
+        this.uncraftedDescription = uncraftedDescription;
         this.row = row;
     }
 
