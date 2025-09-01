b07cdf0f61aee70be82b13418ea6a8532f83467f
Samantha Divina <lavcorps@protonmail.com>
2025-09-01 12:09:28 -0600

    tweak description, details, and timeMessage for time_player

--- Commands/time_player.js
+++ Commands/time_player.js
@@ -2,8 +2,9 @@ const settings = include('Configs/settings.json');
 
 module.exports.config = {
     name: "time_player",
-    description: "Check the current in-game time.",
-    details: "Check the current in-game time.",
+    description: "Shows the current in-game time.",
+    details: "Shows the current in-game time. This will show you the time in the timezone that the bot is currently operating in. "
+        + "This may differ from your local time.",
     usage: `${settings.commandPrefix}time`,
     usableBy: "Player",
     aliases: ["time"]
@@ -13,7 +14,7 @@ module.exports.run = async (bot, game, message, command, args, player) => {
     const status = player.getAttributeStatusEffects("disable time");
     if (status.length > 0) return game.messageHandler.addReply(message, `You cannot do that because you are **${status[0].name}**.`);
 
-    const timeMessage = `The time is **${new Date().toLocaleTimeString()}**`;
+    const timeMessage = `The time is **${new Date().toLocaleTimeString()}**.`;
     game.messageHandler.addGameMechanicMessage(player.member, timeMessage);
 
     return;
