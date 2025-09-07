2abdc8e379d12d93a2780178e85585ef62e541c6
Samantha Divina <lavcorps@protonmail.com>
2025-09-07 14:25:37 -0600

    tweak wake_player.test

--- Test/Commands/wake_player.test.js
+++ Test/Commands/wake_player.test.js
@@ -19,28 +19,28 @@ describe('wake_player command', () => {
 
     test('should cure when conditions met', async () => {
         player.getAttributeStatusEffects.mockReturnValue([]);
-        player.statusString.includes.mockReturnValue(true)
-        await wake_player.run(bot, game, message, 'wake', [], player)
-        expect(game.messageHandler.addReply).not.toHaveBeenCalled()
-        expect(player.cure).toHaveBeenCalled()
+        player.statusString.includes.mockReturnValue(true);
+        await wake_player.run(bot, game, message, 'wake', [], player);
+        expect(game.messageHandler.addReply).not.toHaveBeenCalled();
+        expect(player.cure).toHaveBeenCalled();
     });
 
     test('shouldnt cure when player inflicted with disable wake', async () => {
         const statusEffect = { name: 'tranquilized' };
         player.getAttributeStatusEffects.mockReturnValue([statusEffect]);
-        player.statusString.includes.mockReturnValue(true)
-        await wake_player.run(bot, game, message, 'wake', [], player)
-        const reply = game.messageHandler.addReply.mock.calls[0][1]
-        expect(reply).toBe(`You cannot do that because you are **${statusEffect.name}**.`)
-        expect(player.cure).not.toHaveBeenCalled()
+        player.statusString.includes.mockReturnValue(true);
+        await wake_player.run(bot, game, message, 'wake', [], player);
+        const reply = game.messageHandler.addReply.mock.calls[0][1];
+        expect(reply).toBe(`You cannot do that because you are **${statusEffect.name}**.`);
+        expect(player.cure).not.toHaveBeenCalled();
     });
 
     test('shouldnt cure when player not asleep', async () => {
         player.getAttributeStatusEffects.mockReturnValue([]);
-        player.statusString.includes.mockReturnValue(false)
-        await wake_player.run(bot, game, message, 'wake', [], player)
-        const reply = game.messageHandler.addReply.mock.calls[0][1]
-        expect(reply).toBe("You are not currently asleep.")
-        expect(player.cure).not.toHaveBeenCalled()
+        player.statusString.includes.mockReturnValue(false);
+        await wake_player.run(bot, game, message, 'wake', [], player);
+        const reply = game.messageHandler.addReply.mock.calls[0][1];
+        expect(reply).toBe("You are not currently asleep.");
+        expect(player.cure).not.toHaveBeenCalled();
     });
 });
