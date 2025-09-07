6fffe520da76ecd06642e2496cb35be20851a1f2
Samantha Divina <lavcorps@protonmail.com>
2025-09-07 14:25:16 -0600

    implement sleep_player.test.js

--- Test/Commands/sleep_player.test.js
+++ Test/Commands/sleep_player.test.js
@@ -16,6 +16,23 @@ describe('sleep_player command', () => {
     afterEach(() => {
         jest.clearAllMocks();
     });
-    
-    test('', async () => {});
+
+    test('should inflict when conditions met', async () => {
+        player.getAttributeStatusEffects.mockReturnValue([]);
+        player.statusString.includes.mockReturnValue(true);
+        await sleep_player.run(bot, game, message, 'sleep', [], player);
+        expect(game.messageHandler.addReply).not.toHaveBeenCalled();
+        expect(player.inflict).toHaveBeenCalled();
+        expect(player.setOffline).toHaveBeenCalled();
+    });
+
+    test('shouldnt inflict when player inflicted with disable sleep', async () => {
+        const statusEffect = { name: 'stimulated' };
+        player.getAttributeStatusEffects.mockReturnValue([statusEffect]);
+        await sleep_player.run(bot, game, message, 'sleep', [], player);
+        const reply = game.messageHandler.addReply.mock.calls[0][1];
+        expect(reply).toBe(`You cannot do that because you are **${statusEffect.name}**.`);
+        expect(player.inflict).not.toHaveBeenCalled();
+        expect(player.setOffline).not.toHaveBeenCalled();
+    });
 });
--- Test/Mocks/player.js
+++ Test/Mocks/player.js
@@ -1,7 +1,9 @@
 module.exports.mock = {
     cure: jest.fn(),
     getAttributeStatusEffects: jest.fn(),
+    inflict: jest.fn(),
     member: {},
+    setOffline: jest.fn(),
     statusString: {
         includes: jest.fn()
     }
