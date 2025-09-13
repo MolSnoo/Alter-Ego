4041361f6a94712448ee1f07dc0cdbcbcc739297
Samantha Divina <lavcorps@protonmail.com>
2025-09-13 14:02:24 -0600

    Shim default configuration data & establish include() shim

--- Test/setup.js
+++ Test/setup.js
@@ -1,1 +1,9 @@
-global.include = require('app-root-path').require;
+const _app_root_path_require_ = require('app-root-path').require;
+global.include = (path) => {
+    if (path.startsWith("Configs/")) {
+        const trimmedPath = path.endsWith('.json') ? path.slice(0, -'.json'.length) : path;
+        return _app_root_path_require_(`Test/${trimmedPath}`);
+    } else {
+        return _app_root_path_require_(path);
+    }
+};
