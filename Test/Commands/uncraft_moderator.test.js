58a646ee6d0f943528f522b656b480cfc79392f6
Samantha Divina <lavcorps@protonmail.com>
2025-09-27 14:06:05 -0600

    add uncraft_moderator test stub

---
+++ Test/Commands/uncraft_moderator.test.js
@@ -0,0 +1,17 @@
+const uncraft_moderator = include('Commands/uncraft_moderator');
+
+var botMock = include('Test/Mocks/bot').mock;
+var gameMock = include('Test/Mocks/game').mock;
+var messageMock = include('Test/Mocks/message').mock;
+var playerMock = include('Test/Mocks/player').mock;
+
+describe('uncraft_moderator command', () => {
+    beforeEach(() => {
+        bot = botMock;
+        game = gameMock;
+        message = messageMock;
+        player = playerMock;
+    });
+        
+    test('', async () => {});
+});
