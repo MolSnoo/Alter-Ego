1404462554f0c52fb86d1b789baac27124b9b78e
Samantha Divina <lavcorps@protonmail.com>
2025-09-27 14:04:55 -0600

    begin mocking player inventories

--- Test/Mocks/player.js
+++ Test/Mocks/player.js
@@ -1,3 +1,6 @@
+const InventoryItem = include("Data/InventoryItem");
+const playerdefaults = include('Configs/playerdefaults.json');
+
 module.exports.mock = {
     cure: jest.fn(),
     getAttributeStatusEffects: jest.fn(),
@@ -6,5 +9,20 @@ module.exports.mock = {
     setOffline: jest.fn(),
     statusString: {
         includes: jest.fn()
-    }
+    },
+    inventory: (() => {
+        return playerdefaults.defaultInventory.map(item => 
+            new InventoryItem(
+                module.exports.mock,
+                (item[0] === "NULL") ? null : undefined, // TODO: mock prefabs
+                item[1].replace("#", "1"), // TODO: allow for iterative instantiation, rather than fixed identifiers
+                item[2],
+                (item[3] === "") ? null : undefined, // TODO: handle containers
+                (item[4] && !isNaN(Number(item[4]))) ? Number(item[4]) : null,
+                (item[5] && !isNaN(Number(item[5]))) ? Number(item[5]) : null,
+                item[6],
+                null
+            ) // TODO: provide equippedItem
+        )
+    })()
 };
