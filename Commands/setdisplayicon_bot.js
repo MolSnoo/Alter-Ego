75c07c3d23002dc9c951b12816b32d0c060b331d
LavCorps <LavCorps@protonmail.com>
2025-11-22 16:49:40 -0700

    Expand extension validation to include JPEG, WebP, and AVIF (#250)

--- Commands/setdisplayicon_bot.js
+++ Commands/setdisplayicon_bot.js
@@ -2,11 +2,11 @@ module.exports.config = {
     name: "setdisplayicon_bot",
     description: "Sets a player's display icon.",
     details: "Sets the icon that will display when the given player's dialog appears in spectator channels. It will also appear in Room channels when the "
-        + "player uses the say command. The icon given must be a URL with a .jpg or .png extension. When player data is reloaded, their display icon will "
-        + "be reverted to their Discord avatar. Note that if the player is inflicted  with or cured of a status effect with the concealed attribute, "
-        + "their display icon will be updated, thus overwriting one that was set manually. However, this command can be used to overwrite their new "
-        + "display icon afterwards as well. Note that this command will not change the player's avatar when they send messages to Room channels normally. "
-        + "If you use \"player\" in place of a player's name, then the player who triggered the command will have their display icon changed. "
+        + "player uses the say command. The icon given must be a URL with an extension of .jpg, .jpeg, .png, .webp, or .avif. When player data is reloaded, "
+        + "their display icon will be reverted to their Discord avatar. Note that if the player is inflicted  with or cured of a status effect with the "
+        + "concealed attribute, their display icon will be updated, thus overwriting one that was set manually. However, this command can be used to overwrite "
+        + "their new display icon afterwards as well. Note that this command will not change the player's avatar when they send messages to Room channels "
+        + "normally. If you use \"player\" in place of a player's name, then the player who triggered the command will have their display icon changed. "
         + "To reset a player's display icon to their Discord avatar, simply do not specify a new display icon.",
     usage: `setdisplayicon kyra https://cdn.discordapp.com/attachments/697623260736651335/912103115241697301/mm.png\n`
         + `setdisplayicon player https://cdn.discordapp.com/attachments/697623260736651335/911381958553128960/questionmark.png\n`
@@ -34,13 +34,13 @@ module.exports.run = async (bot, game, command, args, player, data) => {
 
     args.splice(0, 1);
 
-    var input = args.join(" ").replace(/(?<=http(s?))@(?=.*?(jpg|png))/g, ':').replace(/(?<=http(s?):.*?)\\(?=.*?(jpg|png))/g, '/');
-    const iconURLSyntax = RegExp('(http(s?)://.*?.(jpg|png))$');
+    var input = args.join(" ").replace(/(?<=http(s?))@(?=.*?(jpg|jpeg|png|webp|avif))/g, ':').replace(/(?<=http(s?):.*?)\\(?=.*?(jpg|jpeg|png|webp|avif))/g, '/');
+    const iconURLSyntax = RegExp('(http(s?)://.*?.(jpg|jpeg|png|webp|avif))$');
     if (input === "") {
         if (player.talent === "NPC") input = player.id;
         else input = null;
     }
-    else if (!iconURLSyntax.test(input)) return game.messageHandler.addGameMechanicMessage(game.commandChannel, `Error: Couldn't execute command "${cmdString}". The display icon must be a URL with a .jpg or .png extension.`);
+    else if (!iconURLSyntax.test(input)) return game.messageHandler.addGameMechanicMessage(game.commandChannel, `Error: Couldn't execute command "${cmdString}". The display icon must be a URL with an extension of .jpg, .jpeg, .png, .webp, or .avif.`);
 
     player.displayIcon = input;
 
