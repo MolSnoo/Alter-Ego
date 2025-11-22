b0957cd29dd304ba6fa459a5989949c69ca40459
Samantha Divina <lavcorps@protonmail.com>
2025-11-21 19:36:47 -0700

    Initial implementation of bot and moderator versions of setdefaultroomicon

---
+++ Commands/setdefaultroomicon_bot.js
@@ -0,0 +1,27 @@
+const serverconfig = include('Configs/serverconfig.json');
+
+const fs = require('fs');
+
+module.exports.config = {
+    name: "setdefaultroomicon_bot",
+    description: "Sets a room's display icon.",
+    details: "Sets the icon that will display by default when the given room's information is displayed, if there exists no specific icon for that room. "
+        + "The icon given must be a URL with a .jpg, .jpeg, .png, .gif, .webp, or .avif extension. To reset the default icon, simply do not specify a new icon.",
+    usage: `setdefaultroomicon https://media.discordapp.net/attachments/1290826220367249489/1441259427411001455/sLPkDhP.png\n`
+        + `setdefaultroomicon`,
+    usableBy: "Bot",
+    aliases: ["setdefaultroomicon"]
+};
+
+module.exports.run = async (bot, game, command, args, player, data) => {
+    const cmdString = command + " " + args.join(" ");
+
+    var input = args.join(" ").replace(/(?<=http(s?))@(?=.*?(jpg|jpeg|png|gif|webp|avif))/g, ':').replace(/(?<=http(s?):.*?)\\(?=.*?(jpg|jpeg|png|gif|webp|avif))/g, '/');
+    const iconURLSyntax = RegExp('(http(s?)://.*?.(jpg|jpeg|png|gif|webp|avif))$');
+    if (!iconURLSyntax.test(input)) return game.messageHandler.addGameMechanicMessage(game.commandChannel, `Error: Couldn't execute command "${cmdString}". The display icon must be a URL with a .jpg, .jpeg, .png, .gif, .webp, or .avif extension.`);
+
+    let json = JSON.stringify(serverconfig);
+    await fs.writeFileSync('Configs/serverconfig.json', json, 'utf8');
+
+    return;
+};
---
+++ Commands/setdefaultroomicon_moderator.js
@@ -0,0 +1,35 @@
+const settings = include('Configs/settings.json');
+const serverconfig = include('Configs/serverconfig.json');
+
+const fs = require('fs');
+
+module.exports.config = {
+    name: "setdefaultroomicon_moderator",
+    description: "Sets a room's icon.",
+    details: "Sets the icon that will display when the given room's information is displayed. The icon given must be an attachment or URL with a .jpg, "
+        + ".jpeg, .png, .gif, .webp, or .avif extension. To reset a room's icon, simply do not specify a new icon.",
+    usage: `${settings.commandPrefix}setroomicon living-room https://media.discordapp.net/attachments/1290826220367249489/1441259427411001455/sLPkDhP.png\n`
+        + `${settings.commandPrefix}setroomicon kitchen`,
+    usableBy: "Moderator",
+    aliases: ["setdefaultroomicon"]
+};
+
+module.exports.run = async (bot, game, message, command, args) => {
+    const iconURLSyntax = RegExp('(http(s?)://.*?.(jpg|jpeg|png|gif|webp|avif))$');
+    var input = args.join(" ");
+    if (input.length === 0) {
+        if (message.attachments.size === 0)
+            return game.messageHandler.addReply(message, `You must provide a display icon, either as a URL or a file with a .jpg, .jpeg, .png, .gif, .webp, or .avif extension.`);
+        else {
+            input = message.attachments.first().url;
+        }
+    }
+    else if (!iconURLSyntax.test(input)) return game.messageHandler.addReply(message, `The display icon must be a URL with a .jpg, .jpeg, .png, .gif, .webp, or .avif extension.`);
+
+    let json = JSON.stringify(serverconfig);
+    await fs.writeFileSync('Configs/serverconfig.json', json, 'utf8');
+
+    game.messageHandler.addGameMechanicMessage(message.channel, `Successfully updated the default room icon.`);
+
+    return;
+};
