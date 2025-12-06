447b2151ab1527b80610c4b89b502ae5be65403c
LavCorps <LavCorps@protonmail.com>
2025-12-05 21:21:46 -0700

    Implemented type hinting for PriorityQueue and StackQueue (#267)

--- Classes/StackQueue.js
+++ Classes/StackQueue.js
@@ -1,23 +1,54 @@
+/**
+ * @class StackQueue
+ * @classdesc Double-stack queue system for use by the priority queue, used by the message handler.
+ * @constructor
+ */
 export default class StackQueue {
+    /** 
+     * Incoming stack for queued message entries.
+     * @type {Array<MessageQueueEntry>}
+     */
+    inStack;
+    /**
+     * Outgoing stack for queued message entries.
+     * @type {Array<MessageQueueEntry>}
+     */
+    outstack;
+    /**
+     * Length of StackQueue
+     * @type {number}
+     */
+    length;
+
     constructor() {
         this.inStack = [];
         this.outStack = [];
         this.length = 0;
     }
 
+    /**
+     * @param {MessageQueueEntry} value
+     */
     enqueue(value) {
         this.inStack.push(value);
         this.length++;
     }
 
+    /**
+     * @returns {MessageQueueEntry | undefined}
+     */
     dequeue() {
-        if (this.length === 0) return null;
-        if (this.outStack.length === 0) {
-            while (this.inStack.length > 0) {
-                this.outStack.push(this.inStack.pop());
+        if (this.length === 0) {
+            if (this.outStack.length === 0) {
+                while (this.inStack.length > 0) {
+                    this.outStack.push(this.inStack.pop());
+                }
+            }
+            const out = this.outStack.pop();
+            if (out) {
+                this.length--;
+                return out;
             }
         }
-        this.length--;
-        return this.outStack.pop();
     }
 }
--- global.d.ts
+++ global.d.ts
@@ -102,6 +102,13 @@ declare global {
 	}
 
 	/**
+	 * Represents a queue entry for a message waiting to be sent in one of the priority queue's stack queues.
+	 */
+	interface MessageQueueEntry {
+		fire: () => Promise<void>;
+	}
+
+	/**
      * Represents a 3D position.
      * @property x - X coordinate
      * @property y - Y coordinate
