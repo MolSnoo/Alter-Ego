27f437666c9dac63d0949e96113453ec13b8f429
Samantha Divina <lavcorps@protonmail.com>
2025-12-07 14:45:51 -0700

    Initial implementation of mocked configuration data

--- Test/setup.js
+++ Test/setup.js
@@ -1,6 +1,19 @@
 import { afterEach, vi, expect } from 'vitest';
+
+import credentials from './__mocks__/configs/credentials.js';
+import demodata from './__mocks__/configs/demodata.js';
+import playerdefaults from './__mocks__/configs/playerdefaults.js';
+import serverconfig from './__mocks__/configs/serverconfig.js';
+import settings from './__mocks__/configs/settings.js';
+
 import toBeWithinRange from './__extenders__/toBeWithinRange.js';
 import toHaveSize from './__extenders__/toHaveSize.js';
+
+vi.mock('./Configs/credentials.json', () => ({ default: credentials }));
+vi.mock('./Configs/demodata.json', () => ({ default: demodata }));
+vi.mock('./Configs/playerdefaults.json', () => ({ default: playerdefaults }));
+vi.mock('./Configs/serverconfig.json', () => ({ default: serverconfig }));
+vi.mock('./Configs/settings.json', () => ({ default: settings }));
 
 afterEach(() => {
     vi.resetModules();
