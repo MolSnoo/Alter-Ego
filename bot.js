7fc0979e2fdccba5428d57eaa580991e1774254b
Samantha Divina <lavcorps@protonmail.com>
2025-12-16 21:02:11 -0700

    extend dayjs with customParseFormat

--- bot.js
+++ bot.js
@@ -28,10 +28,12 @@ import duration from 'dayjs/plugin/duration.js';
 import weekday from 'dayjs/plugin/weekday.js';
 import advancedFormat from 'dayjs/plugin/advancedFormat.js';
 import relativeTime from 'dayjs/plugin/relativeTime.js';
+import customParseFormat from 'dayjs/plugin/customParseFormat.js';
 dayjs.extend(duration);
 dayjs.extend(weekday);
 dayjs.extend(advancedFormat);
 dayjs.extend(relativeTime);
+dayjs.extend(customParseFormat);
 
 const client = new Client({
     partials: [
