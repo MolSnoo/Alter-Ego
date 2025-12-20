d6ce5da0bbbf1bd9b4d3436de7f248ce7408b716
Samantha Divina <lavcorps@protonmail.com>
2025-12-19 20:57:14 -0700

    begin implementing PrettyPrinter test

--- Test/Classes/PrettyPrinter.test.js
+++ Test/Classes/PrettyPrinter.test.js
@@ -1,5 +1,44 @@
-describe('PrettyPrinter test', () => {
+import { Collection } from "discord.js";
+import PrettyPrinter from "../../Classes/PrettyPrinter.js";
+
+describe("PrettyPrinter test", () => {
     beforeEach(() => {});
-        
-    test('', () => {});
+
+    const objectToPretty = {
+        game: "<SHOULD BE TRUNCATED BY prettyObject>",
+        guild: "<SHOULD BE TRUNCATED BY prettyObject>",
+        member: "<SHOULD BE TRUNCATED BY prettyObject>",
+        channel: "<SHOULD BE TRUNCATED BY prettyObject>",
+        spectateChannel: "<SHOULD BE TRUNCATED BY prettyObject>",
+        timer: "<SHOULD BE TRUNCATED BY prettyObject>",
+        recursion: {
+            recursion: {
+                recursion: {
+                    recursion: {
+                        recursion: {
+                            recursion: {
+                                game: "<SHOULD NOT BE TRUNCATED BY prettyObject>",
+                                guild: "<SHOULD NOT BE TRUNCATED BY prettyObject>",
+                                member: "<SHOULD NOT BE TRUNCATED BY prettyObject>",
+                                channel: "<SHOULD NOT BE TRUNCATED BY prettyObject>",
+                                spectateChannel: "<SHOULD NOT BE TRUNCATED BY prettyObject>",
+                                timer: "<SHOULD NOT BE TRUNCATED BY prettyObject>",
+                            },
+                        },
+                    },
+                },
+            },
+        },
+        aStr: "Hello World!",
+        aNum: 1337,
+        anArray: ["Hello World!", 1337],
+        aMap: new Map([["foo", "bar"]]),
+        aCollection: new Collection([["foo", "bar"]]),
+    };
+
+    test("Expect No Exceptions", () => {
+        const prettyPrinter = new PrettyPrinter();
+        expect(() => prettyPrinter.prettyString(objectToPretty)).not.toThrow();
+        expect(() => prettyPrinter.prettyObject(objectToPretty)).not.toThrow();
+    });
 });
