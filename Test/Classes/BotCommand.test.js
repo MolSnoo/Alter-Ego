db704833010e928c5550debc1cb540b29f70db7e
Samantha Divina <lavcorps@protonmail.com>
2025-12-20 14:18:47 -0700

    Fix missing semicolons in BotCommand test

--- Test/Classes/BotCommand.test.js
+++ Test/Classes/BotCommand.test.js
@@ -9,13 +9,13 @@ describe("BotCommand test", () => {
             async () => {}
         );
 
-        expect(command.config.aliases instanceof Array).toBeTruthy()
-        expect(typeof command.config.description === 'string').toBeTruthy()
-        expect(typeof command.config.details === 'string').toBeTruthy()
-        expect(typeof command.config.name === 'string').toBeTruthy()
-        expect(typeof command.config.requiresGame === 'boolean').toBeTruthy()
-        expect(typeof command.config.usableBy === 'string').toBeTruthy()
-        expect(isAsyncFunction(command.execute)).toBeTruthy()
-        expect(command.usage instanceof Function).toBeTruthy()
+        expect(command.config.aliases instanceof Array).toBeTruthy();
+        expect(typeof command.config.description === "string").toBeTruthy();
+        expect(typeof command.config.details === "string").toBeTruthy();
+        expect(typeof command.config.name === "string").toBeTruthy();
+        expect(typeof command.config.requiresGame === "boolean").toBeTruthy();
+        expect(typeof command.config.usableBy === "string").toBeTruthy();
+        expect(isAsyncFunction(command.execute)).toBeTruthy();
+        expect(command.usage instanceof Function).toBeTruthy();
     });
 });
