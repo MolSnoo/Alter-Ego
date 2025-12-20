e5a09ff3fda122a925acf17d19adfd9b85e2f4cd
Samantha Divina <lavcorps@protonmail.com>
2025-12-20 14:12:27 -0700

    add simple tests for BotCommand, EligibleCommand, ModeratorCommand, and PlayerCommand

--- Test/Classes/BotCommand.test.js
+++ Test/Classes/BotCommand.test.js
@@ -1,5 +1,21 @@
-describe('BotCommand test', () => {
-    beforeEach(() => {});
-        
-    test('', () => {});
+import { isAsyncFunction } from "util/types";
+import BotCommand from "../../Classes/BotCommand.js";
+
+describe("BotCommand test", () => {
+    test("Construct Bot Command", async () => {
+        const command = new BotCommand(
+            { name: "", description: "", details: "", usableBy: "", aliases: [], requiresGame: false },
+            () => "",
+            async () => {}
+        );
+
+        expect(command.config.aliases instanceof Array).toBeTruthy()
+        expect(typeof command.config.description === 'string').toBeTruthy()
+        expect(typeof command.config.details === 'string').toBeTruthy()
+        expect(typeof command.config.name === 'string').toBeTruthy()
+        expect(typeof command.config.requiresGame === 'boolean').toBeTruthy()
+        expect(typeof command.config.usableBy === 'string').toBeTruthy()
+        expect(isAsyncFunction(command.execute)).toBeTruthy()
+        expect(command.usage instanceof Function).toBeTruthy()
+    });
 });
--- Test/Classes/EligibleCommand.test.js
+++ Test/Classes/EligibleCommand.test.js
@@ -1,5 +1,21 @@
-describe('EligibleCommand test', () => {
-    beforeEach(() => {});
-        
-    test('', () => {});
+import { isAsyncFunction } from "util/types";
+import EligibleCommand from "../../Classes/EligibleCommand.js";
+
+describe("EligibleCommand test", () => {
+    test("Construct Eligible Command", async () => {
+        const command = new EligibleCommand(
+            { name: "", description: "", details: "", usableBy: "", aliases: [], requiresGame: false },
+            () => "",
+            async () => {}
+        );
+
+        expect(command.config.aliases instanceof Array).toBeTruthy();
+        expect(typeof command.config.description === "string").toBeTruthy();
+        expect(typeof command.config.details === "string").toBeTruthy();
+        expect(typeof command.config.name === "string").toBeTruthy();
+        expect(typeof command.config.requiresGame === "boolean").toBeTruthy();
+        expect(typeof command.config.usableBy === "string").toBeTruthy();
+        expect(isAsyncFunction(command.execute)).toBeTruthy();
+        expect(command.usage instanceof Function).toBeTruthy();
+    });
 });
