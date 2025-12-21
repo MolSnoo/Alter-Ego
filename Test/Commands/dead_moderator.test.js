d28901aeb56e7e296aa6cefe9a489ba515dd36f7
Samantha Divina <lavcorps@protonmail.com>
2025-12-20 21:31:04 -0700

    implement dead_moderator test as an example of loaded command test

--- Test/Commands/dead_moderator.test.js
+++ Test/Commands/dead_moderator.test.js
@@ -1,7 +1,26 @@
-import { usage, execute, config } from '../../Commands/dead_moderator.js'
+import ModeratorCommand from "../../Classes/ModeratorCommand.js";
+import { usage, execute, config } from "../../Commands/dead_moderator.js";
+import { createMockMessage } from "../__mocks__/libs/discord.js";
+import * as messageHandler from "../../Modules/messageHandler.js";
 
-describe('dead_moderator command', () => {
-    beforeEach(() => {});
-        
-    test('', async () => {});
+describe("dead_moderator command", () => {
+    beforeEach(async () => {
+        await game.entityLoader.loadAll();
+    });
+
+    afterEach(() => {
+        game.entityLoader.clearAll()
+    })
+
+    const dead_moderator = new ModeratorCommand(config, usage, execute);
+
+    test("dead_moderator execution", async () => {
+        // @ts-ignore
+        await dead_moderator.execute(game, createMockMessage(), "dead", []);
+        messageHandler.sendQueuedMessages(game);
+        /** @type {import('vitest').Mock} */
+        // @ts-ignore
+        const sendMock = game.guildContext.commandChannel.send;
+        expect(sendMock).toHaveBeenCalledExactlyOnceWith("Dead players:\nEvad");
+    });
 });
