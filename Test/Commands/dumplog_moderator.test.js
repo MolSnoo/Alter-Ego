e9d5255bd02a6185938915ad68217fa1d0703cbb
Samantha Divina <lavcorps@protonmail.com>
2025-12-20 19:55:41 -0700

    implement simplistic dumplog test

--- Test/Commands/dumplog_moderator.test.js
+++ Test/Commands/dumplog_moderator.test.js
@@ -1,7 +1,19 @@
-import { usage, execute, config } from '../../Commands/dumplog_moderator.js'
+import ModeratorCommand from "../../Classes/ModeratorCommand.js";
+import { usage, execute, config } from "../../Commands/dumplog_moderator.js";
+import { createMockMessage } from "../__mocks__/libs/discord.js";
 
-describe('dumplog_moderator command', () => {
-    beforeEach(() => {});
-        
-    test('', async () => {});
+describe("dumplog_moderator command", () => {
+    const dumplog_moderator = new ModeratorCommand(config, usage, execute);
+
+    test('execute', async () => {
+        const mockMessage = createMockMessage();
+        // @ts-ignore
+        await dumplog_moderator.execute(game, mockMessage, "dumplog", [])
+        expect(game.messageQueue.size()).toStrictEqual(0);
+        expect(game.guildContext.commandChannel.send).toHaveBeenCalledOnce()
+        // @ts-ignore
+        expect(game.guildContext.commandChannel.send.mock.lastCall[0].content).toBe("Successfully generated log files.")
+        // @ts-ignore
+        expect(game.guildContext.commandChannel.send.mock.lastCall[0].files.length).toBe(2)
+    });
 });
