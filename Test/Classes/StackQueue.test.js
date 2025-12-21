a053200e8f1672e8d7d66e3d26640023a919d8f0
Samantha Divina <lavcorps@protonmail.com>
2025-12-20 18:37:08 -0700

    improve StackQueue test

--- Test/Classes/StackQueue.test.js
+++ Test/Classes/StackQueue.test.js
@@ -5,37 +5,14 @@ describe("StackQueue test", () => {
         queue.clear();
     });
 
+    /** @type {StackQueue<number>} */
     const queue = new StackQueue();
 
     test("Usage Test", () => {
         queue.enqueue(1);
-        expect(queue.inStack).toStrictEqual([1]);
-        expect(queue.outStack).toStrictEqual([]);
         queue.enqueue(2);
-        queue.dequeue();
-        expect(queue.inStack).toStrictEqual([]);
-        expect(queue.outStack).toStrictEqual([2]);
-    });
-
-    test("Clear Test", () => {
-        for (let i = 0; i < 10; i++) {
-            queue.enqueue(i);
-        }
-        expect(queue.inStack).toStrictEqual([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
-        expect(queue.outStack).toStrictEqual([]);
-        for (let i = 0; i < 3; i++) {
-            queue.dequeue();
-        }
-        expect(queue.inStack).toStrictEqual([]);
-        expect(queue.outStack).toStrictEqual([9, 8, 7, 6, 5, 4, 3]);
-        for (let i = 10; i < 20; i++) {
-            queue.enqueue(i);
-        }
-        expect(queue.inStack).toStrictEqual([10, 11, 12, 13, 14, 15, 16, 17, 18, 19]);
-        expect(queue.outStack).toStrictEqual([9, 8, 7, 6, 5, 4, 3]);
-        queue.clear();
-        expect(queue.inStack).toStrictEqual([]);
-        expect(queue.outStack).toStrictEqual([]);
+        expect(queue.dequeue()).toStrictEqual(1);
+        expect(queue.dequeue()).toStrictEqual(2);
     });
 
     test("Size Test", () => {
