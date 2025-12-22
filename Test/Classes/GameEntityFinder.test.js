c1f06152dc7bd5448b68c6136f658225471acf14
Amy Poon <amy@amypoon.me>
2025-12-22 00:17:50 -0500

    Added tests for gameEntityFinder (getPlayer, getLivingPlayer, getDeadPlayer, getInventoryItem, getGesture, getFlag, getFlagValue)

--- Test/Classes/GameEntityFinder.test.js
+++ Test/Classes/GameEntityFinder.test.js
@@ -5,6 +5,10 @@ import Prefab from "../../Data/Prefab.js";
 import Puzzle from "../../Data/Puzzle.js";
 import Event from "../../Data/Event.js";
 import Status from "../../Data/Status.js";
+import Player from "../../Data/Player.js";
+import InventoryItem from "../../Data/InventoryItem.js";
+import Gesture from "../../Data/Gesture.js";
+import Flag from "../../Data/Flag.js";
 
 describe("GameEntityFinder test", () => {
     beforeAll(async () => {
@@ -17,6 +21,16 @@ describe("GameEntityFinder test", () => {
     describe("getRoom test", () => {
         test("Get valid room", () => {
             let room = game.entityFinder.getRoom("lobby");
+            expect(room).toBeInstanceOf(Room);
+            expect(room.id).toBe("lobby");
+        });
+        test("Get valid room with whitespace", () => {
+            let room = game.entityFinder.getRoom(" lobby ");
+            expect(room).toBeInstanceOf(Room);
+            expect(room.id).toBe("lobby");
+        });
+        test("Get valid room with mixed case", () => {
+            let room = game.entityFinder.getRoom("LoBby");
             expect(room).toBeInstanceOf(Room);
             expect(room.id).toBe("lobby");
         });
@@ -36,6 +50,16 @@ describe("GameEntityFinder test", () => {
             expect(fixture).toBeInstanceOf(Fixture);
             expect(fixture.name).toBe("RECEPTION DESK");
             expect(fixture.location.id).toBe("lobby");
+        });
+        test("Get valid fixture with whitespace", () => {
+            let fixture = game.entityFinder.getFixture(" RECEPTION DESK ", "lobby");
+            expect(fixture).toBeInstanceOf(Fixture);
+            expect(fixture.name).toBe("RECEPTION DESK");
+        });
+        test("Get valid fixture with mixed case", () => {
+            let fixture = game.entityFinder.getFixture("ReCePtIoN DeSk", "lobby");
+            expect(fixture).toBeInstanceOf(Fixture);
+            expect(fixture.name).toBe("RECEPTION DESK");
         });
         test("Get valid fixture without location", () => {
             let fixture = game.entityFinder.getFixture("RECEPTION DESK");
@@ -174,12 +198,12 @@ describe("GameEntityFinder test", () => {
             expect(puzzle.location.id).toBe("suite-2");
         });
         test("Get valid puzzle by name, location, and type", () => {
-           let puzzle = game.entityFinder.getPuzzle("CALL BUTTON", "floor-2-hall-1", "interact");
-           expect(puzzle).toBeInstanceOf(Puzzle);
-           expect(puzzle.name).toBe("CALL BUTTON");
-           expect(puzzle.location.id).toBe("floor-2-hall-1");
-           expect(puzzle.type).toBe("interact");
-           expect(puzzle.accessible).toBe(true);
+            let puzzle = game.entityFinder.getPuzzle("CALL BUTTON", "floor-2-hall-1", "interact");
+            expect(puzzle).toBeInstanceOf(Puzzle);
+            expect(puzzle.name).toBe("CALL BUTTON");
+            expect(puzzle.location.id).toBe("floor-2-hall-1");
+            expect(puzzle.type).toBe("interact");
+            expect(puzzle.accessible).toBe(true);
         });
         test("Get valid puzzle by name, location, type, and solved status", () => {
             let puzzle = game.entityFinder.getPuzzle("VENT", "suite-2", "interact", false);
@@ -212,19 +236,19 @@ describe("GameEntityFinder test", () => {
     });
 
     describe("getEvent test", () => {
-       test("Get valid event by id", () => {
-           let event = game.entityFinder.getEvent("SNOW");
-           expect(event).toBeInstanceOf(Event);
-           expect(event.id).toBe("SNOW");
-       });
-       test("Get invalid event by id", () => {
-           let event = game.entityFinder.getEvent("INVALID");
-           expect(event).toBeUndefined();
-       });
-       test("Get empty event by id", () => {
-           let event = game.entityFinder.getEvent("");
-           expect(event).toBeUndefined();
-       });
+        test("Get valid event by id", () => {
+            let event = game.entityFinder.getEvent("SNOW");
+            expect(event).toBeInstanceOf(Event);
+            expect(event.id).toBe("SNOW");
+        });
+        test("Get invalid event by id", () => {
+            let event = game.entityFinder.getEvent("INVALID");
+            expect(event).toBeUndefined();
+        });
+        test("Get empty event by id", () => {
+            let event = game.entityFinder.getEvent("");
+            expect(event).toBeUndefined();
+        });
     });
 
     describe("getStatusEffect test", () => {
@@ -240,6 +264,200 @@ describe("GameEntityFinder test", () => {
         test("Get empty status effect by id", () => {
             let statusEffect = game.entityFinder.getStatusEffect("");
             expect(statusEffect).toBeUndefined();
+        });
+    });
+
+    describe("getPlayer test", () => {
+        test("Get valid player by name", () => {
+            let player = game.entityFinder.getPlayer("Amadeus");
+            expect(player).toBeInstanceOf(Player);
+            expect(player.name).toBe("Amadeus");
+        });
+        test("Get valid player with special characters by name", () => {
+            let player = game.entityFinder.getPlayer("???");
+            expect(player).toBeInstanceOf(Player);
+            expect(player.name).toBe("???");
+        })
+        test("Get invalid player by name", () => {
+            let player = game.entityFinder.getPlayer("INVALID");
+            expect(player).toBeUndefined();
+        });
+        test("Get empty player by name", () => {
+            let player = game.entityFinder.getPlayer("");
+            expect(player).toBeUndefined();
+        });
+    });
+
+    describe("getLivingPlayer test", () => {
+        test("Get valid living player by name", () => {
+            let player = game.entityFinder.getLivingPlayer("Amadeus");
+            expect(player).toBeInstanceOf(Player);
+            expect(player.name).toBe("Amadeus");
+            expect(player.alive).toBe(true);
+        });
+        test("Get dead player by name", () => {
+            let player = game.entityFinder.getLivingPlayer("Evad");
+            expect(player).toBeUndefined();
+        });
+        test("Get invalid player by name", () => {
+            let player = game.entityFinder.getLivingPlayer("INVALID");
+            expect(player).toBeUndefined();
+        });
+        test("Get empty player by name", () => {
+            let player = game.entityFinder.getLivingPlayer("");
+            expect(player).toBeUndefined();
+        });
+    });
+
+    describe("getDeadPlayer test", () => {
+        test("Get dead player by name", () => {
+            let player = game.entityFinder.getDeadPlayer("Evad");
+            expect(player).toBeInstanceOf(Player);
+            expect(player.name).toBe("Evad");
+            expect(player.alive).toBe(false);
+        });
+        test("Get living player by name", () => {
+            let player = game.entityFinder.getDeadPlayer("Amadeus");
+            expect(player).toBeUndefined();
+        });
+        test("Get invalid player by name", () => {
+            let player = game.entityFinder.getDeadPlayer("INVALID");
+            expect(player).toBeUndefined();
+        });
+        test("Get empty player by name", () => {
+            let player = game.entityFinder.getDeadPlayer("");
+            expect(player).toBeUndefined();
+        });
+    });
+
+    describe("getInventoryItem test", () => {
+        test("Get valid inventory item by id", () => {
+            let inventoryItem = game.entityFinder.getInventoryItem("KYRAS GLASSES");
+            expect(inventoryItem).toBeInstanceOf(InventoryItem);
+            expect(inventoryItem.player.name).toBe("Kyra");
+            expect(inventoryItem.prefab.id).toBe("KYRAS GLASSES");
+            expect(inventoryItem.identifier).toBe("");
+            expect(inventoryItem.equipmentSlot).toBe("GLASSES");
+        });
+        test("Get valid inventory item by id and player", () => {
+            let inventoryItem = game.entityFinder.getInventoryItem("KYRAS GLASSES", "Kyra");
+            expect(inventoryItem).toBeInstanceOf(InventoryItem);
+            expect(inventoryItem.player.name).toBe("Kyra");
+            expect(inventoryItem.prefab.id).toBe("KYRAS GLASSES");
+            expect(inventoryItem.identifier).toBe("");
+            expect(inventoryItem.equipmentSlot).toBe("GLASSES");
+        });
+        test("Get valid inventory item by id, containerName and player", () => {
+            let inventoryItem = game.entityFinder.getInventoryItem("KYRAS GLASSES", "Kyra", "");
+            expect(inventoryItem).toBeInstanceOf(InventoryItem);
+            expect(inventoryItem.player.name).toBe("Kyra");
+            expect(inventoryItem.prefab.id).toBe("KYRAS GLASSES");
+            expect(inventoryItem.identifier).toBe("");
+            expect(inventoryItem.equipmentSlot).toBe("GLASSES");
+        });
+        test("Get valid inventory item by id, containerName and player", () => {
+            let inventoryItem = game.entityFinder.getInventoryItem("KYRAS GLASSES", "Kyra", "", "GLASSES");
+            expect(inventoryItem).toBeInstanceOf(InventoryItem);
+            expect(inventoryItem.player.name).toBe("Kyra");
+            expect(inventoryItem.prefab.id).toBe("KYRAS GLASSES");
+            expect(inventoryItem.identifier).toBe("");
+            expect(inventoryItem.equipmentSlot).toBe("GLASSES");
+        });
+        test("Get invalid inventory item by id", () => {
+            let inventoryItem = game.entityFinder.getInventoryItem("INVALID");
+            expect(inventoryItem).toBeUndefined();
+        });
+        test("Get invalid inventory item by id and player", () => {
+            let inventoryItem = game.entityFinder.getInventoryItem("INVALID", "Kyra");
+            expect(inventoryItem).toBeUndefined();
+        });
+        test("Get inventory item with invalid player", () => {
+            let inventoryItem = game.entityFinder.getInventoryItem("KYRAS GLASSES", "Vivian");
+            expect(inventoryItem).toBeUndefined();
+        });
+        test("Get inventory item with invalid containerName", () => {
+            let inventoryItem = game.entityFinder.getInventoryItem("KYRAS GLASSES", "Kyra", "INVALID");
+            expect(inventoryItem).toBeUndefined();
+        });
+        test("Get empty inventory item by id", () => {
+            let inventoryItem = game.entityFinder.getInventoryItem("");
+            expect(inventoryItem).toBeUndefined();
+        });
+        test("Get inventory item with empty params", () => {
+            let inventoryItem = game.entityFinder.getInventoryItem("", "", "", "");
+            expect(inventoryItem).toBeUndefined();
+        });
+    });
+
+    describe("getGesture test", () => {
+        test("Get valid gesture by id", () => {
+            let gesture = game.entityFinder.getGesture("smile");
+            expect(gesture).toBeInstanceOf(Gesture);
+            expect(gesture.id).toBe("smile");
+        });
+        test("Get valid gesture by id with whitespace", () => {
+            let gesture = game.entityFinder.getGesture(" smile at                ");
+            expect(gesture).toBeInstanceOf(Gesture);
+            expect(gesture.id).toBe("smile at");
+        });
+        test("Get invalid gesture by id", () => {
+            let gesture = game.entityFinder.getGesture("INVALID");
+            expect(gesture).toBeUndefined();
+        });
+        test("Get empty gesture by id", () => {
+            let gesture = game.entityFinder.getGesture("");
+            expect(gesture).toBeUndefined();
+        });
+    });
+
+    describe("getFlag test", () => {
+        test("Get valid flag by id", () => {
+            let flag = game.entityFinder.getFlag("COLD SEASON FLAG");
+            expect(flag).toBeInstanceOf(Flag);
+            expect(flag.id).toBe("COLD SEASON FLAG");
+        });
+        test("Get valid flag by id with whitespace", () => {
+            let flag = game.entityFinder.getFlag(" COLD SEASON FLAG                ");
+            expect(flag).toBeInstanceOf(Flag);
+            expect(flag.id).toBe("COLD SEASON FLAG");
+        });
+        test("Get invalid flag by id", () => {
+            let flag = game.entityFinder.getFlag("INVALID");
+            expect(flag).toBeUndefined();
+        });
+        test("Get empty flag by id", () => {
+            let flag = game.entityFinder.getFlag("");
+            expect(flag).toBeUndefined();
+        });
+    });
+
+    describe("getFlagValue test", () => {
+        afterEach(async () => {
+            await game.entityLoader.loadAll();
+        });
+        test("Get valid boolean flag value by id", () => {
+            let flagValue = game.entityFinder.getFlagValue("COLD SEASON FLAG");
+            expect(flagValue).toBe(false);
+        });
+        test("Get valid string flag value by id", () => {
+            let flagValue = game.entityFinder.getFlagValue("DAILY ANIMAL");
+            expect(flagValue).toBe("bear");
+        });
+        test("Get valid string flag and evaluate", () => {
+            let flagValue = game.entityFinder.getFlagValue("DAILY ANIMAL", true);
+            expect(flagValue).toBeOneOf(["cat", "dog", "mouse", "owl", "bear"]);
+        });
+        test("Get valid boolean flag and evaluate", () => {
+            let flagValue = game.entityFinder.getFlagValue("DAYTIME", true);
+            expect(flagValue).toBe(true);
+        });
+        test("Get invalid flag value by id", () => {
+            let flagValue = game.entityFinder.getFlagValue("INVALID");
+            expect(flagValue).toBeUndefined();
+        });
+        test("Get empty flag value by id", () => {
+            let flagValue = game.entityFinder.getFlagValue("");
+            expect(flagValue).toBeUndefined();
         });
     });
 
