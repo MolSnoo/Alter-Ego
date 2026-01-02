0a8771cffc49e61a9a1aa90fe1bc16eebc71af2a
Samantha Divina <lavcorps@protonmail.com>
2026-01-01 18:58:16 -0700

    begin implementing unstash player test

--- Test/Commands/unstash_player.test.js
+++ Test/Commands/unstash_player.test.js
@@ -1,7 +1,27 @@
-import { usage, execute, config } from '../../Commands/unstash_player.js'
+import PlayerCommand from "../../Classes/PlayerCommand.js";
+import { usage, execute, config } from "../../Commands/unstash_player.js";
+import UnstashAction from "../../Data/Actions/UnstashAction.js";
+import { clearQueue } from "../../Modules/messageHandler.js";
+import { createMockMessage } from "../__mocks__/libs/discord.js";
 
-describe('unstash_player command', () => {
-    beforeEach(() => {});
-        
-    test('', async () => {});
+describe("unstash_player command", () => {
+    beforeEach(async () => {
+        await game.entityLoader.loadAll();
+    });
+
+    afterEach(() => {
+        game.entityLoader.clearAll();
+        clearQueue(game);
+        vi.resetAllMocks();
+    });
+
+    const unstash_player = new PlayerCommand(config, usage, execute);
+
+    test("valid item from valid container", async () => {
+        const player = game.entityFinder.getPlayer("Kyra");
+        const spy = vi.spyOn(UnstashAction.prototype, "performUnstash");
+        // @ts-ignore
+        await unstash_player.execute(game, createMockMessage(), "retrieve", ["hamburger", "bun", "from", "pack", "of", "toilet", "paper"], player);
+        expect(spy).toHaveBeenCalled() // TODO: not called?
+    });
 });
