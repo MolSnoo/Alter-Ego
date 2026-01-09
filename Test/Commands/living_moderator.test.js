4cdcd537d8c42c125e5a7ca123cdb1419314cff4
Samantha Divina <lavcorps@protonmail.com>
2026-01-09 15:22:17 -0700

    implement living_moderator test

--- Test/Commands/living_moderator.test.js
+++ Test/Commands/living_moderator.test.js
@@ -1,7 +1,26 @@
-import { usage, execute, config } from '../../Commands/living_moderator.js'
+import ModeratorCommand from "../../Classes/ModeratorCommand.js";
+import { usage, execute, config } from "../../Commands/living_moderator.js";
+import { createMockMessage } from "../__mocks__/libs/discord.js";
+import { sendQueuedMessages } from "../../Modules/messageHandler.js";
 
-describe('living_moderator command', () => {
-    beforeEach(() => {});
-        
-    test('', async () => {});
+describe("living_moderator command", () => {
+    beforeEach(async () => {
+        await game.entityLoader.loadAll();
+    });
+
+    afterEach(() => {
+        game.entityLoader.clearAll()
+    })
+
+    const living_moderator = new ModeratorCommand(config, usage, execute);
+
+    test("living_moderator execution", async () => {
+        // @ts-ignore
+        await living_moderator.execute(game, createMockMessage(), "living", []);
+        sendQueuedMessages(game);
+        /** @type {import('vitest').Mock} */
+        // @ts-ignore
+        const sendMock = game.guildContext.commandChannel.send;
+        expect(sendMock).toHaveBeenCalledExactlyOnceWith("Living players:\nKyra Vivian Astrid Nero Asuka Luna Kiara Amadeus ???");
+    });
 });
