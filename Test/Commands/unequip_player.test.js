a7f267ab9d7008a611fadda982f39a47ff644b70
Samantha Divina <lavcorps@protonmail.com>
2026-01-08 18:34:19 -0700

    begin implementing unequip_player test

--- Test/Commands/unequip_player.test.js
+++ Test/Commands/unequip_player.test.js
@@ -1,7 +1,28 @@
+import PlayerCommand from '../../Classes/PlayerCommand.js';
 import { usage, execute, config } from '../../Commands/unequip_player.js'
+import UnequipAction from '../../Data/Actions/UnequipAction.js';
+import { clearQueue } from '../../Modules/messageHandler.js';
+import { createMockMessage } from '../__mocks__/libs/discord.js';
 
 describe('unequip_player command', () => {
-    beforeEach(() => {});
+    beforeEach(async () => {
+        await game.entityLoader.loadAll();
+    });
+
+    afterEach(() => {
+        game.entityLoader.clearAll();
+        clearQueue(game);
+        vi.resetAllMocks();
+    });
+
+    const unequip_player = new PlayerCommand(config, usage, execute);
         
-    test('', async () => {});
+    test('given valid item', async () => {
+        const player = game.entityFinder.getPlayer("Kyra");
+        const item = game.entityFinder.getInventoryItem("kyras glasses", "Kyra");
+        const spy = vi.spyOn(UnequipAction.prototype, "performUnequip");
+        // @ts-ignore
+        await unequip_player.execute(game, createMockMessage(), "unequip", ["glasses"], player);
+        expect(spy).toHaveBeenCalled();
+    });
 });
