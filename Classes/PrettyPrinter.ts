e6997c974e166af1798727a53d8890fcba1fcf47
LavCorps <lavcorps@protonmail.com>
2026-07-26 17:38:50 -0600

    add copyright info

--- Classes/PrettyPrinter.ts
+++ Classes/PrettyPrinter.ts
@@ -1,3 +1,8 @@
+// SPDX-FileCopyrightText: 2019 Alter Ego Contributors
+// SPDX-FileCopyrightText: 2026 LavCorps <lavcorps@protonmail.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 import { Collection, Guild, GuildMember, TextChannel, DMChannel } from 'discord.js';
 import { format } from 'pretty-format';
 import { Duration } from 'luxon';
