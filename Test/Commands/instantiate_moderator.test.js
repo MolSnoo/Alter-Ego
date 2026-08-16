85a5119c8668438b98dc246c0b4b2ea00acfda42
LavCorps <lavcorps@protonmail.com>
2026-08-15 21:49:00 -0600

    more tests

--- Test/Commands/instantiate_moderator.test.js
+++ Test/Commands/instantiate_moderator.test.js
@@ -17,23 +17,7 @@ import { createMockModerator } from '../__mocks__/utility.ts';
 
 /**
  * @privateRemarks
- * a few tests still need implementation, and nested describes would help clean up the test structure...
- * inventory:
- *   - invalid item with procedural selections into player hand
- *     (should error on the same case as invalid item without procedural selections into player hand)
- *   - valid item with invalid procedural selection possibility containing items with valid procedural selections into player hand
- *     (should error on the same case as valid item with invalid procedural selection possibility into player hand)
- *   - valid item with invalid procedural selection containing items with valid procedural selections into player hand
- *     (should error on the same case as valid item with invalid procedural selection into player hand)
- *   - valid item with invalid procedural selection possibility containing items with invalid procedural selection possibility into player hand
- *     (should error on the same case as valid item with invalid procedural selection possibility into player hand)
- *   - valid item with invalid procedural selection containing items with invalid procedural selection possibility into player hand
- *     (should error on the same case as valid item with invalid procedural selection into player hand)
- *   - valid item with invalid procedural selection possibility containing items with invalid procedural selection into player hand
- *     (should error on the same case as valid item with invalid procedural selection possibility into player hand)
- *   - valid item with invalid procedural selection containing items with invalid procedural selection into player hand
- *     (should error on the same case as valid item with invalid procedural selection into player hand)
- * all tests should be repeated for room item instantiation as well.
+ * TODO: all inventory tests should be repeated for room item instantiation.
  * -AC
  */
 
@@ -189,6 +173,16 @@ describe('instantiate_moderator command', () => {
                 expect(context).toBeUndefined();
                 expect(message.reply).toBeInvokedWith("Couldn't find prefab with id \"SOMETHING VERY SCARY\".");
             });
+
+            test('invalid item with procedural selections into player hand', async () => {
+                const message = createMockMessage({ channel: testGame.guildContext.commandChannel });
+                // @ts-expect-error
+                await instantiate_moderator.execute(testGame, message, "create", ["something", "very", "scary", "(scary", "=", "true)", "in", "kyra's", "left", "hand"], moderator);
+                await testGame.messageQueue.process();
+                expect(spy).not.toHaveBeenCalled();
+                expect(context).toBeUndefined();
+                expect(message.reply).toBeInvokedWith("Couldn't find prefab with id \"SOMETHING VERY SCARY\".");
+            });
         });
 
         describe('invalid invocations (procedural)', () => {
@@ -248,6 +242,156 @@ describe('instantiate_moderator command', () => {
                 expect(spy).not.toHaveBeenCalled();
                 expect(context).toBeUndefined();
                 expect(message.reply).toBeInvokedWith("PEN does not have procedural \"scary\".");
+            });
+
+            test('valid item with invalid procedural selection possibility containing items with valid procedural selections into player hand', async () => {
+                const message = createMockMessage({ channel: testGame.guildContext.commandChannel });
+                const args = [
+                    "fired", "glazed", "clay", "pot",
+                        "(base", "color", "=", "obscured", "+",
+                        "quality", "=", "excellent", "+",
+                        "glaze", "color", "=", "black", "+",
+                        "pattern", "=", "drip", "lines", "+",
+                        "pattern", "quality", "=", "ornate", "+",
+                        "pattern", "color", "=", "rainbow)",
+                    "containing",
+                        "pen", "(ink", "color", "=", "red)", "+",
+                        "pen", "(ink", "color", "=", "green)", "+",
+                        "pen", "(ink", "color", "=", "blue)",
+                    "in",
+                        "kyra's", "left", "hand",
+                ];
+                // @ts-expect-error
+                await instantiate_moderator.execute(testGame, message, "create", args, moderator);
+                await testGame.messageQueue.process();
+                expect(spy).not.toHaveBeenCalled();
+                expect(context).toBeUndefined();
+                expect(message.reply).toBeInvokedWith("FIRED GLAZED CLAY POT's procedural \"pattern color\" does not have possibility \"rainbow\".");
+            });
+
+            test('valid item with invalid procedural selection containing items with valid procedural selections into player hand', async () => {
+                const message = createMockMessage({ channel: testGame.guildContext.commandChannel });
+                const args = [
+                    "fired", "glazed", "clay", "pot",
+                        "(base", "color", "=", "obscured", "+",
+                        "quality", "=", "excellent", "+",
+                        "glaze", "color", "=", "black", "+",
+                        "pattern", "=", "drip", "lines", "+",
+                        "pattern", "quality", "=", "ornate", "+",
+                        "scary", "=", "true)",
+                    "containing",
+                        "pen", "(ink", "color", "=", "red)", "+",
+                        "pen", "(ink", "color", "=", "green)", "+",
+                        "pen", "(ink", "color", "=", "blue)",
+                    "in",
+                        "kyra's", "left", "hand",
+                ];
+                // @ts-expect-error
+                await instantiate_moderator.execute(testGame, message, "create", args, moderator);
+                await testGame.messageQueue.process();
+                expect(spy).not.toHaveBeenCalled();
+                expect(context).toBeUndefined();
+                expect(message.reply).toBeInvokedWith("FIRED GLAZED CLAY POT does not have procedural \"scary\".");
+            });
+
+            test('valid item with invalid procedural selection possibility containing items with invalid procedural selection possibility into player hand', async () => {
+                const message = createMockMessage({ channel: testGame.guildContext.commandChannel });
+                const args = [
+                    "fired", "glazed", "clay", "pot",
+                        "(base", "color", "=", "obscured", "+",
+                        "quality", "=", "excellent", "+",
+                        "glaze", "color", "=", "black", "+",
+                        "pattern", "=", "drip", "lines", "+",
+                        "pattern", "quality", "=", "ornate", "+",
+                        "pattern", "color", "=", "rainbow)",
+                    "containing",
+                        "pen", "(ink", "color", "=", "rainbow)", "+",
+                        "pen", "(ink", "color", "=", "green)", "+",
+                        "pen", "(ink", "color", "=", "red)",
+                    "in",
+                        "kyra's", "left", "hand",
+                ];
+                // @ts-expect-error
+                await instantiate_moderator.execute(testGame, message, "create", args, moderator);
+                await testGame.messageQueue.process();
+                expect(spy).not.toHaveBeenCalled();
+                expect(context).toBeUndefined();
+                expect(message.reply).toBeInvokedWith("FIRED GLAZED CLAY POT's procedural \"pattern color\" does not have possibility \"rainbow\".");
+            });
+
+            test('valid item with invalid procedural selection possibility containing items with invalid procedural selection into player hand', async () => {
+                const message = createMockMessage({ channel: testGame.guildContext.commandChannel });
+                const args = [
+                    "fired", "glazed", "clay", "pot",
+                        "(base", "color", "=", "obscured", "+",
+                        "quality", "=", "excellent", "+",
+                        "glaze", "color", "=", "black", "+",
+                        "pattern", "=", "drip", "lines", "+",
+                        "pattern", "quality", "=", "ornate", "+",
+                        "pattern", "color", "=", "rainbow)",
+                    "containing",
+                        "pen", "(scary", "=", "true)", "+",
+                        "pen", "(ink", "color", "=", "green)", "+",
+                        "pen", "(ink", "color", "=", "blue)",
+                    "in",
+                        "kyra's", "left", "hand",
+                ];
+                // @ts-expect-error
+                await instantiate_moderator.execute(testGame, message, "create", args, moderator);
+                await testGame.messageQueue.process();
+                expect(spy).not.toHaveBeenCalled();
+                expect(context).toBeUndefined();
+                expect(message.reply).toBeInvokedWith("FIRED GLAZED CLAY POT's procedural \"pattern color\" does not have possibility \"rainbow\".");
+            });
+
+            test('valid item with invalid procedural selection containing items with invalid procedural selection possibility into player hand', async () => {
+                const message = createMockMessage({ channel: testGame.guildContext.commandChannel });
+                const args = [
+                    "fired", "glazed", "clay", "pot",
+                        "(base", "color", "=", "obscured", "+",
+                        "quality", "=", "excellent", "+",
+                        "glaze", "color", "=", "black", "+",
+                        "pattern", "=", "drip", "lines", "+",
+                        "pattern", "quality", "=", "ornate", "+",
+                        "scary", "=", "true)",
+                    "containing",
+                        "pen", "(ink", "color", "=", "rainbow)", "+",
+                        "pen", "(ink", "color", "=", "green)", "+",
+                        "pen", "(ink", "color", "=", "red)",
+                    "in",
+                        "kyra's", "left", "hand",
+                ];
+                // @ts-expect-error
+                await instantiate_moderator.execute(testGame, message, "create", args, moderator);
+                await testGame.messageQueue.process();
+                expect(spy).not.toHaveBeenCalled();
+                expect(context).toBeUndefined();
+                expect(message.reply).toBeInvokedWith("FIRED GLAZED CLAY POT does not have procedural \"scary\".");
+            });
+
+            test('valid item with invalid procedural selection containing items with invalid procedural selection into player hand', async () => {
+                const message = createMockMessage({ channel: testGame.guildContext.commandChannel });
+                const args = [
+                    "fired", "glazed", "clay", "pot",
+                        "(base", "color", "=", "obscured", "+",
+                        "quality", "=", "excellent", "+",
+                        "glaze", "color", "=", "black", "+",
+                        "pattern", "=", "drip", "lines", "+",
+                        "pattern", "quality", "=", "ornate", "+",
+                        "scary", "=", "true)",
+                    "containing",
+                        "pen", "(scary", "=", "true)", "+",
+                        "pen", "(ink", "color", "=", "green)", "+",
+                        "pen", "(ink", "color", "=", "blue)",
+                    "in",
+                        "kyra's", "left", "hand",
+                ];
+                // @ts-expect-error
+                await instantiate_moderator.execute(testGame, message, "create", args, moderator);
+                await testGame.messageQueue.process();
+                expect(spy).not.toHaveBeenCalled();
+                expect(context).toBeUndefined();
+                expect(message.reply).toBeInvokedWith("FIRED GLAZED CLAY POT does not have procedural \"scary\".");
             });
         });
     });
